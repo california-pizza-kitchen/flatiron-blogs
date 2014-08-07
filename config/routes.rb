e8c7a7511b4ff88caa94754643c4675ed5f91859
pcrglennon <pcrglennon@gmail.com>
2014-08-07 14:17:26 -0400

    Initial template for semester index page, add index route/Rails controller action, add link to navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   root to: 'entries#index'
 
-  resources :semesters, :param => :slug, only: [:show]
+  resources :semesters, :param => :slug, only: [:index, :show]
   resources :bloggers, only: [:index]
 
   get '/:slug', to: 'entries#show'
-
 end
