3665e672a1e93b0e28e8a36870dd4dc3dc10756c
pcrglennon <pcrglennon@gmail.com>
2014-08-07 14:15:26 -0400

    Rename schoolSession JS model to semester, and add properties

---
+++ app/assets/javascripts/models/semester.js
@@ -0,0 +1,27 @@
+function Semester(json) {
+  this.name = json["name"];
+  this.slug = json["slug"];
+  this.jsonUrl = json["_self"];
+  this.bloggersCount = json["bloggers_count"];
+  this.entriesCount = json["entries_count"];
+  if (json["bloggers"]) {
+    this.bloggers = this.makeBloggers(json["bloggers"]);
+  }
+  if (json["entries"]) {
+    this.entries = this.makeEntries(json["entries"]);
+  }
+}
+
+Semester.prototype.makeEntries = function(jsonEntries) {
+  var entries = $.map(jsonEntries, function(jsonEntry, i) {
+    return new Entry(jsonEntry);
+  });
+  return entries;
+}
+
+Semester.prototype.makeBloggers = function(jsonBloggers) {
+  var bloggers = $.map(jsonBloggers, function(jsonEntry, i) {
+    return new Entry(jsonEntry);
+  });
+  return bloggers;
+}
