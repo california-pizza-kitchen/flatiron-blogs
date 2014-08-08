0853840514a3350445a03b0e25a17f9cf7531fd6
pcrglennon <pcrglennon@gmail.com>
2014-08-08 12:00:06 -0400

    Initial sidebar entry styling, add timeAgo function to Entry model

--- app/assets/javascripts/models/entry.js
+++ app/assets/javascripts/models/entry.js
@@ -22,6 +22,9 @@ Entry.prototype.addTags = function(jsonTags) {
   return tags;
 }
 
+Entry.prototype.timeAgo = function() {
+  return jQuery.timeago(this.publishedDate);
+}
 Entry.prototype.formattedDateTime = function() {
   var hours = this.publishedDate.getHours() % 12,
       minutes = this.publishedDate.getMinutes(),
