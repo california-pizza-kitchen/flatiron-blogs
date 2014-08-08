6d148b5e79e01913b19af58862c5be2e09d13137
pcrglennon <pcrglennon@gmail.com>
2014-08-08 13:38:03 -0400

    Add about page + StaticController to handle route to /about

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: 'entries#index'
   get '/blogroll', to: 'bloggers#index'
+  get '/about', to: 'static#about'
 
   resources :semesters, :param => :slug, only: [:index, :show]
 
