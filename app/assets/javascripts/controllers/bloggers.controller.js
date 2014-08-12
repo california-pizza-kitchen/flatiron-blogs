8a462f11624e395da50a8e35ce2171681dd6b86f
Chris Callahan <callahan.c@gmail.com>
2014-08-11 21:36:50 -0400

    increase limit to 20 to allow for all 11 semesters

--- app/assets/javascripts/controllers/bloggers.controller.js
+++ app/assets/javascripts/controllers/bloggers.controller.js
@@ -1,6 +1,6 @@
 function BloggersController() {
   this.$pageContentWrapper = $('#page-content-wrapper');
-  this.baseApiUrl = 'http://flatiron-magazine-cpk.herokuapp.com/api/v0/school_sessions?bloggers=true';
+  this.baseApiUrl = 'http://flatiron-magazine-cpk.herokuapp.com/api/v0/school_sessions?bloggers=true&limit=20';
 }
 
 BloggersController.prototype.fetchSemesters = function() {
